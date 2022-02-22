110b3f95a6d94ae903fcf6a8e5de89efc952d650
Ricardosantos31 <riacrdok9@gmail.com>
2022-02-22 17:10:57 -0400

    Metodo CREATE implementado para categoria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,25 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
-import {MatIconModule} from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 import { HomeComponent } from './components/views/home/home.component';
-import {MatCardModule} from '@angular/material/card';
-import {MatTableModule} from '@angular/material/table';
-import {MatButtonModule} from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
 import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
+import { CategoriaCreateComponent } from './components/views/categoria/categoria-create/categoria-create.component';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+
+
 
 
 
@@ -30,6 +38,7 @@ import { CategoriaReadComponent } from './components/views/categoria/categoria-r
     NavComponent,
     HomeComponent,
     CategoriaReadComponent,
+    CategoriaCreateComponent,
     
   ],
   imports: [
@@ -43,7 +52,11 @@ import { CategoriaReadComponent } from './components/views/categoria/categoria-r
     MatCardModule,
     MatTableModule,
     HttpClientModule,
-    MatButtonModule
+    MatButtonModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/views/categoria/categoria-create/categoria-create.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Categoria } from '../categoria-read/categoria.modelo';
+import { CategoriaService } from '../categoria.service';
+
+@Component({
+  selector: 'app-categoria-create',
+  templateUrl: './categoria-create.component.html',
+  styleUrls: ['./categoria-create.component.css']
+})
+export class CategoriaCreateComponent implements OnInit {
+
+  categoria: Categoria = {
+    nome: '',
+    descricao:''
+  }
+
+  constructor(private service: CategoriaService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  create() : void{
+      this.service.create(this.categoria).subscribe((resposta) => {
+        this.router.navigate(['categorias'])  
+        this.service.mensagem('Categoria criada com sucesso !');
+      }, err => {
+        console.log(err)
+        for (let i = 0; i < err.error.erros.length; i++){
+          this.service.mensagem(err.error.erros[i].messege)
+        }
+      })
+  }
+
+}
